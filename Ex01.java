7fd6000fe51c306afc3d6a5442f569dd186cc6e6
Anderson Ferreira <98371064+dadaFerreira@users.noreply.github.com>
2022-02-19 17:48:45 -0300

    Add files via upload

---
+++ Ex01.java
@@ -0,0 +1,42 @@
+package com.ifood.iFood.adapter;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        boolean flafNumeroValido = false;
+
+        do{
+            System.out.println("Entre com o número de linhas:");
+            int numeroLinhas = scan.nextInt();
+
+            if(numeroLinhas <= 0){
+                System.out.println("Número inválido, digite um número maior que 0.");
+            }else {
+                System.out.println(montaArvore(numeroLinhas));
+                flafNumeroValido = true;
+            }
+
+        }while (flafNumeroValido == false);
+    }
+
+    public static String montaArvore(Integer numeroLinhas){
+
+        String espaco = " ";
+        String caractere = "*";
+        String arvore = "";
+
+        for (int i = 1; i <= numeroLinhas; i++) {
+            arvore += espaco.repeat(numeroLinhas -i);
+            arvore += caractere.repeat(i);
+            arvore +=("\n");
+        }
+
+        return arvore;
+    }
+
+
+}
---
+++ Ex02.java
@@ -0,0 +1,40 @@
+package com.ifood.iFood.adapter;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Cadastre sua senha:");
+        String senha = scan.next();
+
+        System.out.println("Senha válida? " + senhaValida(senha));
+        System.out.println(numeroMinimo(senha));
+
+    }
+
+    public static boolean senhaValida(String senha) {
+
+        String regex = "(?=.*[)(!$@^#&%+\\-*\\-+])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,}";
+        Pattern p = Pattern.compile(regex);
+
+        if (senha == null) {
+            return false;
+        }
+
+        Matcher m = p.matcher(senha);
+        return m.matches();
+    }
+
+    public static int numeroMinimo(String senha){
+
+        int tamanho  = senha.length();
+        return 6 - tamanho;
+    }
+
+}
